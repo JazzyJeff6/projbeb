9bf0d423147cd74290cb18ff90a6a4752deee0a2
Vinícius Moro Ribeiro <82912986+thememec@users.noreply.github.com>
2025-08-12 04:57:39 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const drinksController = require('./controllers/drinksController');
+const registrarController = require('./controllers/registrarController');
 
 app.use(express.static(__dirname));
 
@@ -12,6 +13,8 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 app.post('/buscar', drinksController.buscarDrink);
+
+app.post('/registrar', registrarController.registrarLogin);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
